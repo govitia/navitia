5c324e40a9c819fa0d44e6d2efa54ca0be2152b8
Alexandre A. Bizri <alexandre@bizri.fr>
2017-04-12 00:08:38 +0200

    Fix Date management, including parseDateTime and adding the DateTimeFormat and DateFormat

--- types/json.go
+++ types/json.go
@@ -5,7 +5,15 @@ package types
 import (
 	"fmt"
 	"github.com/pkg/errors"
+	"strings"
 	"time"
+)
+
+const (
+	// DateTimeFormat is the format used by the Navitia Api for use with time pkg.
+	DateTimeFormat string = "20060102T150405" // YYYYMMDDThhmmss
+	// DateFormat is when there is no time info
+	DateFormat string = "20060102"
 )
 
 // parseDateTime parses a time formatted under iso-date-time as indicated in the Navitia api.
@@ -13,11 +21,20 @@ import (
 // If the given string is empty (i.e ""), then the zero value of time.Time will be returned
 func parseDateTime(datetime string) (time.Time, error) {
 	// If there's no datetime given, just return the zero value
-	if datetime == "" {
+	if datetime == "" || datetime == "not-a-date-time" {
 		return time.Time{}, nil
 	}
 
-	res, err := time.Parse(DateTimeFormat, datetime)
+	// If the datetime doesn't countain a "T", then it does not have time info
+	var format string
+	if strings.Contains(datetime, "T") {
+		format = DateTimeFormat
+	} else {
+		format = DateFormat
+	}
+
+	// Parse it
+	res, err := time.Parse(format, datetime)
 	if err != nil {
 		err = errors.Wrap(err, "parseDateTime: error while parsing datetime")
 	}
