a6d2ced5f29e042f7014dd1f8b0cd5c15a505fa6
Alexandre A. Bizri <alexandre@bizri.fr>
2017-04-12 21:40:55 +0200

    Add testing for Places

---
+++ places_test.go
@@ -0,0 +1,35 @@
+package gonavitia
+
+import (
+	"github.com/aabizri/gonavitia/types"
+	"testing"
+)
+
+func Test_Places(t *testing.T) {
+	if *apiKey == "" {
+		t.Skip(skipNoKey)
+	}
+
+	params := PlacesRequest{
+		Query: "avenue",
+	}
+
+	// Run a simple search
+	t.Run("simple", func(t *testing.T) {
+		res, err := testSession.Places(params)
+		t.Logf("Got results: \n%s", res.String())
+		if err != nil {
+			t.Fatalf("Got error in Places(): %v\n\tParameters: %#v", err, params)
+		}
+	})
+
+	// Run a search with proximity
+	t.Run("proximity", func(t *testing.T) {
+		params.Around = types.Coordinates{48.847002, 2.377310}
+		res, err := testSession.Places(params)
+		t.Logf("Got results: \n%s", res.String())
+		if err != nil {
+			t.Fatalf("Got error in Places(): %v\n\tParameters: %#v", err, params)
+		}
+	})
+}
