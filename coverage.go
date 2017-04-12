e58c1587b983862c1e3004d2104070503de2b36a
Alexandre A. Bizri <alexandre@bizri.fr>
2017-04-12 21:38:14 +0200

    Upgrade Coverage to use the new request method

--- coverage.go
+++ coverage.go
@@ -1,11 +1,9 @@
 package gonavitia
 
 import (
-	"encoding/json"
 	"github.com/aabizri/gonavitia/types"
-	"github.com/pkg/errors"
+	"net/url"
 	"strconv"
-	"time"
 )
 
 const coverageEndpoint string = "coverage"
@@ -19,127 +17,69 @@ type CoverageResults struct {
 	Paging Paging `json:"Pagination"`
 
 	// Timing information
-	createdAt   time.Time
-	populatedAt time.Time
+	Logging
 
 	// Held session
 	session *Session
+}
+
+type coverageRequest struct {
+	Count uint
+}
+
+func (req coverageRequest) toURL() (url.Values, error) {
+	params := url.Values{}
+
+	if count := req.Count; count != 0 {
+		countStr := strconv.FormatUint(uint64(count), 10)
+		params["count"] = []string{countStr}
+	}
+
+	return params, nil
+}
+
+func (s *Session) coverage(url string, params coverageRequest) (*CoverageResults, error) {
+	var results = &CoverageResults{session: s}
+	err := s.request(url, params, results)
+	return results, err
 }
 
 // Coverage lists the areas covered by the Navitia API
 // count is the number of items to return, if count=0, then it will return the default number
 // BUG: Count doesn't work, server-side
 func (s *Session) Coverage(count uint) (*CoverageResults, error) {
-	var results = &CoverageResults{session: s, createdAt: time.Now()}
-
-	// Get the request
+	// Create the URL
 	url := s.APIURL + "/" + coverageEndpoint
-	req, err := s.newRequest(url)
-	if err != nil {
-		return results, err
-	}
 
-	// Add the parameters if count != 0
-	if count != 0 {
-		params := req.URL.Query()
-		countStr := strconv.FormatUint(uint64(count), 10)
-		params.Add("count", countStr)
-		req.URL.RawQuery = params.Encode()
-	}
+	// Create the query
+	params := coverageRequest{Count: count}
 
-	// Execute the request
-	resp, err := s.client.Do(req)
-
-	// Check it
-	if err != nil {
-		return results, errors.Wrap(err, "errror while executing request")
-	}
-	if resp.StatusCode != 200 {
-		return results, parseRemoteError(resp)
-	}
-
-	// Parse it
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(results)
-	if err != nil {
-		return results, errors.Wrap(err, "JSON decoding failed")
-	}
-	results.populatedAt = time.Now()
-
-	// Return
-	return results, nil
+	// Call and return
+	return s.coverage(url, params)
 }
 
 // RegionByID provides information about a specific region
 // If the ID provided isn't an ID of a region, this WILL fail.
 func (s *Session) RegionByID(id types.ID) (*CoverageResults, error) {
-	var results = &CoverageResults{session: s, createdAt: time.Now()}
-
 	// Build the URL
 	url := s.APIURL + "/" + coverageEndpoint + "/" + string(id)
 
-	// Get the request
-	req, err := s.newRequest(url)
-	if err != nil {
-		return results, err
-	}
+	// Create the query
+	params := coverageRequest{}
 
-	// Execute the request
-	resp, err := s.client.Do(req)
-
-	// Check it
-	if err != nil {
-		return results, errors.Wrap(err, "errror while executing request")
-	}
-	if resp.StatusCode != 200 {
-		return results, parseRemoteError(resp)
-	}
-
-	// Parse it
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(results)
-	if err != nil {
-		return results, errors.Wrap(err, "JSON decoding failed")
-	}
-	results.populatedAt = time.Now()
-
-	// Return
-	return results, nil
+	// Call and return
+	return s.coverage(url, params)
 }
 
 // RegionByPos provides information about the region englobing the specific position
 func (s *Session) RegionByPos(coords types.Coordinates) (*CoverageResults, error) {
-	var results = &CoverageResults{session: s, createdAt: time.Now()}
-
 	// Build the URL
 	coordsFormatted := coords.QueryEscape()
 	url := s.APIURL + "/" + coverageEndpoint + "/" + coordsFormatted
 
-	// Get the request
-	req, err := s.newRequest(url)
-	if err != nil {
-		return results, errors.Wrap(err, "error while preparing new request")
-	}
+	// Create the query
+	params := coverageRequest{}
 
-	// Execute the request
-	resp, err := s.client.Do(req)
-
-	// Check it
-	if err != nil {
-		return results, errors.Wrap(err, "errror while executing request")
-	}
-	if resp.StatusCode != 200 {
-		return results, parseRemoteError(resp)
-	}
-
-	// Parse it
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(results)
-	if err != nil {
-		return results, errors.Wrap(err, "JSON decoding failed")
-	}
-	results.populatedAt = time.Now()
-
-	// Return
-	return results, nil
+	// Call and return
+	return s.coverage(url, params)
 }
