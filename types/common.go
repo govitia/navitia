d05bfb08733e4d0a95fc576aa695c74a0af12e83
Alexandre A. Bizri <alexandre@bizri.fr>
2017-04-12 15:51:54 +0200

    Add package-level documentation for gonavitia/types

--- types/common.go
+++ types/common.go
@@ -1,3 +1,29 @@
+/*
+Package types implements support for the types used in the Navitia API (see doc.navitia.io), simplified and modified for idiomatic Go use.
+
+This package was and is developped as a supporting library for the gonavitia API client (https://github.com/aabizri/gonavitia) but can be used to build other API clients.
+
+This support includes or will include, for each type.
+	- JSON Unmarshalling via UnmarshalJSON(b []byte), in the format of the navitia.io API
+	- Validity Checking via Check()
+	- Pretty-printing via String()
+
+This package is still a work in progress. It is not API-Stable, and won't be until the v1 release.
+
+Currently supported types
+	- Journey ["journey"]
+	- Section ["section"]
+	- Region ["region"]
+	- Place (This is an interface for your ease-of-use, which is implemented by the five following types)
+	- Address ["address"]
+	- StopPoint ["stop_point"]
+	- StopArea ["stop_area"]
+	- AdministrativeRegion ["administrative_region"]
+	- POI ["poi"]
+	- Line ["line"]
+	- Route ["route"]
+	- And others, such as DisplayInformations ["display_informations"], PTDateTime ["pt-date-time"], StopTime ["stop_time"], Coordinates ["coord"].
+*/
 package types
 
 // DataFreshness codes for a specific data freshness requirement: realtime or base_schedule
