2307a04085c3995fea81243ceaf50df46bcb2a5d
Alexandre A. Bizri <alexandre@bizri.fr>
2017-04-21 01:53:04 +0200

    Add TrafficReport

---
+++ trafficreport.go
@@ -0,0 +1,17 @@
+package types
+
+// A TrafficReport made of a network, an array of lines and an array of stop_areas.
+// Named "traffic_report" in the Navitia doc
+//
+// See http://doc.navitia.io/#traffic-reports
+// TODO: Add the internal links
+type TrafficReport struct {
+	// Main object (network) and links within its own disruptions
+	Network Network
+
+	// List of all disrupted Lines from the network
+	Lines []Line
+
+	// List of all disrupted StopAreas from the network
+	StopAreas []StopArea
+}
