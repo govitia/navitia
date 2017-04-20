7ebff43ba13b9cfc80be4fac22a82098678beaf3
Alexandre A. Bizri <alexandre@bizri.fr>
2017-04-20 20:40:27 +0200

    Put (*Container).UnmarshalJSON into its own file (container_json.go) for clarity

--- container.go
+++ container.go
@@ -48,48 +48,6 @@ type Container struct {
 	embeddedObject Object
 }
 
-func (c *Container) UnmarshalJSON(b []byte) error {
-	// Unmarshal into a map
-	data := map[string]json.RawMessage{}
-	err := json.Unmarshal(b, &data)
-	if err != nil {
-		return errors.Wrap(err, "Couldn't unmarshal into a map")
-	}
-
-	// From a map, extract the ID, Name & EmbeddedType
-	if id, ok := data["id"]; ok {
-		err := json.Unmarshal(id, &c.ID)
-		if err != nil {
-			return err
-		}
-	}
-	if name, ok := data["name"]; ok {
-		err := json.Unmarshal(name, &c.Name)
-		if err != nil {
-			return err
-		}
-	}
-	if embeddedType, ok := data["embedded_type"]; ok {
-		err := json.Unmarshal(embeddedType, &c.EmbeddedType)
-		if err != nil {
-			return err
-		}
-	}
-	if quality, ok := data["quality"]; ok {
-		err := json.Unmarshal(quality, &c.Quality)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Now, unmarshal the last one
-	if embedded, ok := data[c.EmbeddedType]; ok {
-		c.embeddedJSON = embedded
-	}
-
-	return nil
-}
-
 // ErrInvalidContainer is returned after a check on a Container
 type ErrInvalidContainer struct {
 	// If the Container has a zero ID.
