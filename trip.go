90e16be3b55efcb243bb3adfd6e33562e12d75ef
Alexandre A. Bizri <alexandre@bizri.fr>
2017-04-20 22:51:04 +0200

    Add PTObject and move Trip to ptobject.go

--- trip.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// A trip corresponds to a scheduled vehicle circulation (and all its linked real-time and disrupted routes).
-//
-// An example : a train, routing a Paris to Lyon itinerary every day at 06h29, is the “Trip” named “6641”.
-type Trip struct {
-	ID   ID
-	Name string
-}
